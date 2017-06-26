f62a6ac6ceebcdc5241ae378e84e2b865b934b91
Anjali Bhatt <ambhatt@stanford.edu>
2017-06-26 15:00:54 -0400

    adding files

---
+++ analyses/twitter_streaming_AB.R
@@ -0,0 +1,34 @@
+# Cultural Variation by Geography
+# June 2017
+# Pull Arkansas Tweets
+
+setwd("~/Git/sicss-culturalvariation")
+library(rtweet)
+
+# ## Do this to save twitter token for further use
+# twitter_token <- create_token(
+#   app = appname,
+#   consumer_key = key,
+#   consumer_secret = secret)
+# 
+# ## path of home directory
+# home_directory <- path.expand("~/")
+# 
+# ## combine with name for token
+# file_name <- file.path(home_directory, "twitter_token.rds")
+# 
+# ## save token to home directory
+# saveRDS(twitter_token, file = file_name)
+# 
+# cat(paste0("TWITTER_PAT=", file_name),
+#     file = file.path(home_directory, ".Renviron"),
+#     append = TRUE)
+#
+# ## IMPORTANT: RESTART R/RSTUDIO
+
+twitter_token <- get_tokens()
+
+
+stream_tweets(q = "", timeout = 30, parse = TRUE, token = NULL,
+              file_name = NULL, gzip = FALSE, verbose = TRUE, fix.encoding = TRUE,
+              ...)
