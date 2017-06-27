fbdea4ee72a44d83ebbd97455e57d2f314065bc2
Molly Lewis <mollyllewis@gmail.com>
2017-06-27 09:09:58 -0400

    more iowa data

--- analyses/iowa_tweets.R
+++ analyses/iowa_tweets.R
@@ -8,12 +8,12 @@ library(tidyverse)
 twitter_token <- get_tokens()
 
 # Iowa bounding box
-iowa_bounding_box = c(-96.6397171020508, 40.3755989074707, -90.1400604248047, 43.5011367797852)
-
+iowa_bounding_box = c(-96.6397171020508, 40.3755989074707, # southwest coordinates
+                      -90.1400604248047, 43.5011367797852) # north coordinates
 
 ## stream 3 random samples of tweets
-n.tweets <- 500
-for (i in seq_len(n.tweets)) {
+n.tweets <- 1000
+for (i in  500:1500) {
   stream_tweets(q = iowa_bounding_box, timeout = 60,
                 file_name = paste0("iowa_tweets/rtw_iowa", i), parse = FALSE)
   if (i == n.tweets) {
