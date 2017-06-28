f5a6a3fb70e35ab18b72fb03b6b24b60837006c8
Anjali Bhatt <ambhatt@stanford.edu>
2017-06-28 13:21:55 -0400

    keywords

---
+++ analyses/keywords_AB.R
@@ -0,0 +1,53 @@
+# Keywords for google review search
+
+types <- c("church","school","nonprofit",
+              "clinic","salon","auto","pet","veterinary","pharmacy",
+              "gardening","hardware",""
+              "grocery","liquor","supermarket")
+              theater | bakery
+<- c("bank","bakery","beauty_salon","church","clothing_store",
+     "convenience_store","department_store","hardware_store",
+     "store","university","veterinary_care")
+
+types_tourist <- c("bar","cafe","restaurant")
+
+
+bakery
+bank
+beauty_salon
+car_dealer
+car_rental
+car_repair
+car_wash
+church
+clothing_store
+convenience_store
+department_store
+doctor
+electronics_store
+florist
+furniture_store
+gas_station
+gym
+hair_care
+hardware_store
+home_goods_store
+hospital
+jewelry_store
+laundry
+lawyer
+library
+liquor_store
+lodging
+movie_theater
+park
+pet_store
+pharmacy
+real_estate_agency
+school
+shoe_store
+shopping_mall
+spa
+store
+university
+veterinary_care
