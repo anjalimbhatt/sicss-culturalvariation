80fd188d4f0bae5a9e250fc3312b94d9c3ce36bd
anjalimbhatt <ambhatt@stanford.edu>
2017-06-28 18:21:22 -0400

    uploading reviews, stuff for server

--- google_reviews/pre-processing/get_reviews_server_2.R
+++ google_reviews/pre-processing/get_reviews_server_2.R
@@ -3,6 +3,7 @@
 ###################################
 
 # libraries
+install.packages(c("googleway","tidyverse","stringr","feather"))
 library(googleway)
 library(tidyverse)
 library(stringr)
@@ -27,31 +28,31 @@ get_our_stores <- function(place_id, apikey, pause_length){
   price.level = details$result$price_level
   
   no.reviews <- ifelse(is.null(all.reviews), TRUE, FALSE)
-
+  
   df = data.frame(place.id = place_id,
-                 reviews = ifelse(no.reviews, NA, list(all.reviews)),
-                 author.names = ifelse(no.reviews, NA, list(author.names)),
-                 address = ifelse(is.null(address), NA, address),
-                 place.lat = ifelse(is.null(place.lat), NA, place.lat),
-                 place.lon = ifelse(is.null(place.lon), NA, place.lon),
-                 price.level = ifelse(is.null(price.level), NA, price.level))
+                  reviews = ifelse(no.reviews, NA, list(all.reviews)),
+                  author.names = ifelse(no.reviews, NA, list(author.names)),
+                  address = ifelse(is.null(address), NA, address),
+                  place.lat = ifelse(is.null(place.lat), NA, place.lat),
+                  place.lon = ifelse(is.null(place.lon), NA, place.lon),
+                  price.level = ifelse(is.null(price.level), NA, price.level))
   names(df)[2:3] = c("reviews", "author.names") # ugh this shouldn't be necessary
   df
 }
 
 ### read in all files of place ids
-all_city_files <- list.files("../data/place_ids/")[474:946]
+all_city_files <- list.files("../place_ids/")[100:473]
 
 ### loop over cities, get reviews for each place id, and write to feather file
-for (i in 24:length(all_city_files)) {
+for (i in 1:length(all_city_files)) {
   
   # get city name
   city_name = unlist(str_split(all_city_files[i], "_"))[1]
   print(paste0("########## ", city_name, " ##########"))
   
   # get all place ids for this city
-  ids = read_feather(paste0("../data/place_ids/", all_city_files[i]))
-
+  ids = read_feather(paste0("../place_ids/", all_city_files[i]))
+  
   # get reviews for all place ids
   places_with_reviews = ids$place.id %>%
     map(get_our_stores, APIKEY, PAUSE_LENGTH) %>%
@@ -59,5 +60,5 @@ for (i in 24:length(all_city_files)) {
   
   # write to reviews to feather
   write_feather(places_with_reviews, 
-                paste0("../data/reviews/", city_name, "_reviews"))
+                paste0("../reviews/", city_name, "_reviews"))
 }
