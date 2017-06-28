60aa96bfa028c87f7c3eb2ea1f9a5041c5c20109
Molly Lewis <mollyllewis@gmail.com>
2017-06-27 21:28:37 -0400

    ready to collect data, I think

---
+++ iowa_tweets/Untitled.R
@@ -0,0 +1,52 @@
+get_our_places <- function(lat, lon, location, radius, apikey, pause_length){ 
+  lat = as.numeric(lat)
+  lon = as.numeric(lon)
+  
+  MAX_PAGES <- 4
+  current.page = 1
+  token = "first"
+  
+  all.places = data.frame(place.id = NA,
+                          name = NA,
+                          types = NA,
+                          latitude = NA,
+                          longitude = NA,
+                          location = NA)
+  
+  # first page
+  while(!is.null(token) & current.page < MAX_PAGES) {
+        print("hi")
+        page = list(google_places(radius = radius,
+                                   location = c(lat, lon),  
+                                   key = apikey)) 
+        
+        if(is.null(names(page))) {page = purrr::flatten(page)}
+
+        Sys.sleep(runif(1, 0, pause_length))
+        
+        places = as.data.frame(cbind(place.id = page$results$place_id,
+                                        name = unlist(page$results$name),
+                                        types =  page$results$types)) 
+        
+        if(current.page == 1){
+          all.places = places
+          print(all.places)
+        } else {
+          all.places = bind_rows(all.places, places)
+        }
+        
+        token = page$next_page_token 
+        current.page = current.page + 1
+}
+  
+all.places
+  
+}
+
+x <- 1
+while(x < 5) {
+  x <- x+1 
+  #if (x == 3) next; 
+    print(x)
+  }
+
